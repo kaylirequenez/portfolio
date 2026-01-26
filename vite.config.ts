58a73dae7979558666a93387198841089e3daa9f
Kayli Requenez <kayli@Kaylis-MacBook-Pro.local>
2026-01-25 23:41:43 -0500

    changes chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Increase limit to 2MB (default is 500KB)
+  },
 })
